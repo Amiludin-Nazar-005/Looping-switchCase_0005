e81ab45f31e28eab271154543cb0226019208f50
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-03-17 11:01:51 +0700

    membuat variable

---
+++ contoh_looping.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    // perulangan for
+
+
+}
