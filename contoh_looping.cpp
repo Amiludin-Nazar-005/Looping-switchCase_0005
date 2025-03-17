3080c55937f87354f4c035d7da5a5233e120923d
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-03-17 11:05:54 +0700

    perulangan for pencacah naik,turun

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -12,5 +12,14 @@ int main()
 
     // perulangan for
 
+    cout << "Perulangan for pencecah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "nilai terakhir = " << n << endl;
+    cout << endl;
 
+    cout << "Perulangan for pencacah turun" << endl;
+    
 }
