f15f13f2704d69341879b49ac41b471f35cdf52b
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-03-17 12:06:53 +0700

    membuat do untuk while

--- conroh swtich cash/switchCase.cpp
+++ conroh swtich cash/switchCase.cpp
@@ -13,4 +13,40 @@ int main() {
     int pilihan;
     float panjang, lebar;
     float jejari;
+
+
+do{
+    cout << "Menu" << endl;
+    cout << "1. luas persegi panjang" << endl;
+    cout << "2. luas lingkarang" << endl;
+    cout << "3. keluar" << endl;
+
+    cout << "Pilihan (1/2/3) = ";
+    cin >> pilihan;
+
+    switch (pilihan)
+    {
+        case 1:
+            cout << "Hitung luas persegi panjang " << endl;
+            cout << "Masukkan panjang = ";
+            cin >> panjang;
+            cout << "Masukkan lebar = ";
+            cin >> lebar;
+            cout << "Luas persegi panjang = " << luasPersegiPanjang(panjang, lebar);
+            break;
+        case 2:
+            cout << "Hitung luas lingkaran" << endl;
+            cout << "Masukkan jari-jari = ";
+            cin >> jejari;
+            cout << "Luas lingkaran = " << luasLingkarang(jejari) << endl;
+            break;
+        case 3:
+            break;
+        default:
+            cout << "Pilihan salah, pilihan harus di antara 1-3";
+            break;
+
+
+    }
+}while(pilihan != 3);
 }
